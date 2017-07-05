1274063f055ce810293aa1fdc982fbabf6597b5d
sd <s@d.de>
2017-07-05 12:00:48 +0200

    Ente+

---
+++ Ente.java
@@ -0,0 +1,12 @@
+
+public class Ente extends Tier
+{
+    public Ente(String name)
+    {
+        super(name);
+    }
+    
+    public String lautGeben() {
+        return "Quak Quak Quak!";
+    }
+}
