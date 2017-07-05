fb041e923aa913101e4267750009932c1fa4ff77
ts <t@s.de>
2017-07-05 11:53:50 +0200

    Frosch ergaenzt

---
+++ Frosch.java
@@ -0,0 +1,13 @@
+
+public class Frosch extends Tier
+{
+    public Frosch(String name)
+    {
+        super(name);
+    }
+    
+    public String lautGeben() {
+        return "QUAK!";
+    }
+}
+
